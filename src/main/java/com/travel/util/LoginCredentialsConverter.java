2c24889f11b14967a9057c137316366e519fbc83
thesangita <rupaidas498@gmail.com>
2023-09-25 20:47:59 +0530

    converter class updated

---
+++ src/main/java/com/travel/util/LoginCredentialsConverter.java
@@ -0,0 +1,32 @@
+package com.travel.util;
+
+import org.springframework.beans.BeanUtils;
+import com.travel.dto.LoginCredentialsDto;
+import com.travel.entity.LoginCredentials;
+
+public class LoginCredentialsConverter {
+	
+	//converts LoginCredentials Dto to LoginCredentials Entity
+	public LoginCredentials convertDtoToLoginCredEntity(LoginCredentialsDto lcDto)
+	{
+		LoginCredentials lc = new LoginCredentials();
+		
+		if(lcDto!=null)
+		{
+			BeanUtils.copyProperties(lcDto, lc);
+		}	
+		return lc;
+	}
+	
+	//converts LoginCredentials Entity to LoginCredentials Dto
+	public LoginCredentialsDto convertEntityToLoginCredDto(LoginCredentials lc)
+	{
+		LoginCredentialsDto lcDto = new LoginCredentialsDto();
+		
+		if(lc!=null)
+		{
+			BeanUtils.copyProperties(lc, lcDto);
+		}	
+		return lcDto;
+	}
+}
